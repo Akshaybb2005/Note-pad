14d4ae96c90b1db62327d8c2016e687276d6d6eb
Akshay <akshaykumarbb058@gmail.com>
2026-01-29 20:31:38 +0530

    workflows

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,5 @@
 export default {
   testEnvironment: "node",
-  clearMocks: true,
   testMatch: ["**/tests/**/*.test.js"],
-  collectCoverageFrom: [
-    "**/*.js",
-    "!node_modules/**",
-    "!tests/**",
-    "!prisma/**"
-  ],
+  clearMocks: true,
 };
